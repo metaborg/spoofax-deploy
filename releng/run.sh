352a99bb520ab23adffb42a2eb64c04912d00580
Maarten Sijm <mpsijm@gmail.com>
2020-10-20 16:13:19 +0200

    ./b command: Replace virtualenv with venv

--- releng/run.sh
+++ releng/run.sh
@@ -14,16 +14,22 @@ else
   exit 1
 fi
 
-# Ensure that virtualenv is installed and updated
-$PYTHON_CMD -m pip install --quiet --user --upgrade --requirement "$DIR/requirements_virtualenv.txt"
+VENV="$DIR/.venv"
 
-# Create and activate virtualenv
-VENV="$DIR/.virtualenv"
-$PYTHON_CMD -m virtualenv --quiet "$VENV" > /dev/null
-# Since virtualenv is made by idiots, temporarily disable unbound variable checks when activating virtualenv
-set +o nounset
+# Create virtual environment
+if [[ ! -d "$VENV" ]]
+then
+    if ! $PYTHON_CMD -m venv "$VENV" > /dev/null
+    then
+        # On Ubuntu/Debian, Python3 does not automatically come with ensurepip installed, so we install it manually:
+        # https://bugs.launchpad.net/ubuntu/+source/python3.4/+bug/1290847/+index?comments=all (comment #58)
+        $PYTHON_CMD -m venv --without-pip "$VENV" > /dev/null
+        curl -s https://bootstrap.pypa.io/get-pip.py | "$VENV/bin/python" > /dev/null
+    fi
+fi
+
+# Activate virtual environment
 source "$VENV/bin/activate"
-set -o nounset
 
 # Install requirements
 $PYTHON_CMD -m pip install --quiet --requirement "$DIR/requirements.txt"
