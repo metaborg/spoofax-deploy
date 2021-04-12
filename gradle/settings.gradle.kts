51f9bccc53ddf13bf462a6b26b58e898012c13b0
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2021-04-12 23:51:29 +0200

    Add scopegraph projects to build.

--- gradle/settings.gradle.kts
+++ gradle/settings.gradle.kts
@@ -38,6 +38,9 @@ fun String.includeProject(id: String, dir: String = id, path: String = "../../$t
 }
 
 "nabl".run {
+  includeProject("scopegraph")
+  includeProject("p_raffrayi")
+
   includeProject("nabl2.terms")
   includeProject("nabl2.lang")
   includeProject("nabl2.runtime")
