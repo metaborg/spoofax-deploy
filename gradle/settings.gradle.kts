c607c535f0b58b932dfb42ee184f9140f015deb5
Gabriel Konat <gabrielkonat@gmail.com>
2021-06-04 11:36:32 +0000

    Merge branch 'master' into spoofax-release

--- gradle/settings.gradle.kts
+++ gradle/settings.gradle.kts
@@ -10,54 +10,22 @@ if(org.gradle.util.VersionNumber.parse(gradle.gradleVersion).major < 6) {
   enableFeaturePreview("GRADLE_METADATA")
 }
 
-fun String.includeProject(id: String, dir: String = id, path: String = "../../$this/$dir") {
-  val projectDir = file(path)
-  if(projectDir.exists()) {
-    include(id)
-    project(":$id").projectDir = projectDir
+
+// Only include composite builds when this is the root project (it has no parent). Otherwise, the parent project
+// (devenv) will include these composite builds, as IntelliJ does not support nested composite builds.
+if(gradle.parent == null) {
+  includeBuildWithName("java", "spoofax2.releng.java.root")
+  includeBuildWithName("language", "spoofax2.releng.language.root")
+}
+
+fun includeBuildWithName(dir: String, name: String) {
+  includeBuild(dir) {
+    try {
+      ConfigurableIncludedBuild::class.java
+        .getDeclaredMethod("setName", String::class.java)
+        .invoke(this, name)
+    } catch(e: NoSuchMethodException) {
+      // Running Gradle < 6, no need to set the name, ignore.
+    }
   }
-}
-
-"jsglr".run {
-  includeProject("org.spoofax.jsglr")
-  includeProject("org.spoofax.jsglr2")
-  includeProject("org.spoofax.interpreter.library.jsglr")
-}
-
-"sdf".run {
-  includeProject("org.metaborg.parsetable")
-  includeProject("sdf2table", "org.metaborg.sdf2table")
-  includeProject("org.metaborg.meta.lang.template")
-}
-
-"stratego".run {
-  includeProject("org.metaborg.meta.lang.stratego")
-  includeProject("stratego.build")
-  includeProject("stratego.build.spoofax2")
-  includeProject("stratego.compiler.pack")
-}
-
-"nabl".run {
-  includeProject("scopegraph")
-  includeProject("p_raffrayi")
-
-  includeProject("nabl2.terms")
-  includeProject("nabl2.lang")
-  includeProject("nabl2.runtime")
-  includeProject("nabl2.shared")
-
-  includeProject("statix.solver")
-  includeProject("statix.generator")
-  includeProject("statix.lang")
-  includeProject("statix.runtime")
-  
-  includeProject("renaming.java")
-}
-
-"spoofax2".run {
-  includeProject("org.metaborg.core")
-  includeProject("org.metaborg.core.test")
-  includeProject("org.metaborg.spoofax.core")
-  includeProject("org.metaborg.meta.core")
-  includeProject("org.metaborg.spoofax.meta.core")
 }
