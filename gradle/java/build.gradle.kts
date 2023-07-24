a15498e2805cb33b1fa4a13bb2c651003c6d181a
Daniel A. A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2023-07-24 17:37:06 +0000

    Add Spoofax 2 devenv version param

--- gradle/java/build.gradle.kts
+++ gradle/java/build.gradle.kts
@@ -25,8 +25,11 @@ subprojects {
 
 val spoofax2Version: String = System.getProperty("spoofax2Version")
 val spoofax2BaselineVersion: String = System.getProperty("spoofax2BaselineVersion")
+val spoofax2DevenvVersion: String = System.getProperty("spoofax2DevenvVersion")
 allprojects {
   group = "org.metaborg.devenv"
   ext["spoofax2Version"] = spoofax2Version
   ext["spoofax2BaselineVersion"] = spoofax2BaselineVersion
+  ext["spoofax2DevenvVersion"] = spoofax2DevenvVersion
 }
+
