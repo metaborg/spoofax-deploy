3fe982c37f1090f61746891a4bf8310824ba7bc7
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-07-28 22:55:16 +0200

    Update maven-publish convention plugin

--- gradle/java/build.gradle.kts
+++ gradle/java/build.gradle.kts
@@ -1,4 +1,4 @@
-import org.metaborg.convention.Developer
+import org.metaborg.convention.Person
 import org.metaborg.convention.MavenPublishConventionExtension
 
 // Workaround for issue: https://youtrack.jetbrains.com/issue/KTIJ-19369
@@ -28,11 +28,11 @@ allprojects {
             metadata {
                 inceptionYear.set("2008")
                 developers.set(listOf(
-                    Developer("Gohla", "Gabriel Konat", "gabrielkonat@gmail.com"),
-                    Developer("lennartcl", "Lennart Kats", "lclkats@gmail.com"),
-                    Developer("hendrikvanantwerpen", "Hendrik van Antwerpen", "hendrik@van-antwerpen.net"),
-                    Developer("Apanatshka", "Jeff Smits", "mail@jeffsmits.net"),
-                    Developer("Virtlink", "Daniel A. A. Pelsmaeker", "developer@pelsmaeker.net"),
+                    Person("Gohla", "Gabriel Konat", "gabrielkonat@gmail.com"),
+                    Person("lennartcl", "Lennart Kats", "lclkats@gmail.com"),
+                    Person("hendrikvanantwerpen", "Hendrik van Antwerpen", "hendrik@van-antwerpen.net"),
+                    Person("Apanatshka", "Jeff Smits", "mail@jeffsmits.net"),
+                    Person("Virtlink", "Daniel A. A. Pelsmaeker", "developer@pelsmaeker.net"),
                 ))
             }
         }
